37ee2667dccdcff58954a1a826d2b3c2a52b9fef
Rysertio <86291572+Rysertio@users.noreply.github.com>
2023-02-22 23:32:30 +0600

    removed unecessary dependencies

--- biology/bio3.py
+++ biology/bio3.py
@@ -4,7 +4,22 @@
 import random
 import os
 from time import sleep
-import getch 
+
+if name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
 ATP_Values = [["NADH",3],
               ["FADH",2],
